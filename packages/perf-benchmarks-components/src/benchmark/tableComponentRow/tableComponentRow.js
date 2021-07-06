6163cb9c2f33774ce7a00ba00746e434895013f7
Jose David Rodriguez Velasco <jodarove@gmail.com>
2021-07-06 14:49:04 -0700

    wip: add extra getter

--- packages/perf-benchmarks-components/src/benchmark/tableComponentRow/tableComponentRow.js
+++ packages/perf-benchmarks-components/src/benchmark/tableComponentRow/tableComponentRow.js
@@ -7,7 +7,16 @@
 import { LightningElement, api } from 'lwc';
 
 export default class TableComponentRow extends LightningElement {
-    @api row;
+    _row;
+
+    @api
+    get row() {
+        return this._row;
+    }
+
+    set row(v) {
+        this._row = v;
+    }
 
     handleSelect() {
         this.dispatchEvent(new CustomEvent('select'));
